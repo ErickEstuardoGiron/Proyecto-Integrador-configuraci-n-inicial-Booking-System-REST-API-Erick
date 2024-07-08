2f3010ccc4eacd5091d218c92235c241e978ed64
ErickEstuardoGiron <ekarlluvi123@gmail.com>
2024-07-07 21:12:34 -0600

    Erick Controller Swagger

---
+++ src/test/java/com/erick/test/Swagger/SwaggerApp.java
@@ -0,0 +1,12 @@
+package com.erick.test.Swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApp.class, args);
+    }
+}
